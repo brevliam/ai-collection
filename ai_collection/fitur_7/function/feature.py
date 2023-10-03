b658b6fe7dafba98b11a432851d7263d376ae149
brevliam <97146830+brevliam@users.noreply.github.com>
2023-10-03 10:06:11 +0700

    fitur-7: implement feature.py and utils.py

--- ai_collection/fitur_7/function/feature.py
+++ ai_collection/fitur_7/function/feature.py
@@ -1,32 +1,46 @@
+# This module contains every method for inference, as well as data transformation and preprocessing.
+
 from ..apps import Fitur7Config
+from ..libraries import utils
+
 import pandas as pd
 
 def predict_workload_score(data):
-   model = Fitur7Config.workload_pred_model
-   input_df = transform_input(data)
-   output = model.predict(input_df)
-   result = transform_workload_pred_output(output)
-
-   return result
+  model = Fitur7Config.workload_pred_model
+  DATASET_FILE_NAME = 'workload_prediction_v3_230927.csv'
+  
+  input_df = transform_input(data)
+  output = model.predict(input_df)
+  result = transform_workload_pred_output(output)
+  
+  utils.append_dataset_with_new_data(DATASET_FILE_NAME, input_df, result)
+  
+  return result
 
 def transform_input(data):
-    data = {key: [value] for key, value in data.items()}
-    df = pd.DataFrame(data)
-    return df
+  data = {key: [value] for key, value in data.items()}
+  df = pd.DataFrame(data)
+  
+  return df
 
 def transform_workload_pred_output(pred):
-    workload_score = pred[0]
-    workload_level = ''
-    if workload_score < 500:
-      workload_level = 'underload'
-    elif workload_score > 700:
-      workload_level = 'overload'
-    else:
-      workload_level = 'normal'
+  workload_score = pred[0]
+  if workload_score > 1000:
+      workload_score = 1000.0
+  elif workload_score < 0:
+      workload_score == 0.0
 
-    data = {
-        'workload_score': workload_score,
-        'workload_level': workload_level
-    }
+  workload_level = ''
+  if workload_score < 500:
+    workload_level = 'underload'
+  elif workload_score > 700:
+    workload_level = 'overload'
+  else:
+    workload_level = 'normal'
 
-    return data
+  data = {
+      'workload_score': workload_score,
+      'workload_level': workload_level
+  }
+
+  return data
--- ai_collection/fitur_7/views.py
+++ ai_collection/fitur_7/views.py
@@ -9,7 +9,16 @@ class WorkloadPrediction(APIView):
     def post(self, request, format=None):
         try:
             result = predict_workload_score(request.data)
-            return Response(result, status = status.HTTP_200_OK)
+            return Response(build_result(result), status = status.HTTP_200_OK)
         except Exception as e:
             error_message = str(e)
             return Response({'error': error_message}, status=status.HTTP_400_BAD_REQUEST)
+        
+def build_result(result):
+    result = {
+        "status": 200,
+        "message": "success",
+        "result": result
+    }
+
+    return result
