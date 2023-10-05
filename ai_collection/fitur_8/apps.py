0bad744bdc7d5b23e37a1f08f9fa2c60ead34e55
AnugrahAidinYotolembah <anugrahaidinyotolembah19@gmail.com>
2023-10-05 09:07:25 +0700

    fitur-8

--- ai_collection/fitur_8/apps.py
+++ ai_collection/fitur_8/apps.py
@@ -1,6 +1,12 @@
 from django.apps import AppConfig
+import joblib
+import os
 
 
 class Fitur8Config(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'fitur_8'
+
+     # Load models
+    Effecientcosteffectiveness_pred_model_path = os.path.join(os.path.dirname(__file__), 'model', 'adaboost_model_pipeline.joblib')
+    Effecientcosteffectiveness_model = joblib.load(Effecientcosteffectiveness_pred_model_path)
