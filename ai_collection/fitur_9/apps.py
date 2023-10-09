16c8077f52be43c732e257447fb52213ee506cbe
0KNV1 <ahmadalfaruq098@gmail.com>
2023-10-09 10:03:54 +0700

    upload fitur 9

--- ai_collection/fitur_9/apps.py
+++ ai_collection/fitur_9/apps.py
@@ -1,6 +1,15 @@
 from django.apps import AppConfig
-
+from pathlib import Path
+import joblib
+import os
 
 class Fitur9Config(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'fitur_9'
+    
+    default_solution_model_path = os.path.join(os.path.dirname(__file__), 'model', 'kredit_recommendation_solution_v4.joblib.joblib')
+    default_solution_scaler_path = os.path.join(os.path.dirname(__file__), 'model', 'kredit_recommendation_solution_scaler_v4.joblib.joblib')
+    
+    default_solution_model = joblib.load(default_solution_model_path)
+    default_solution_scaler = joblib.load(default_solution_scaler_path)
+    
