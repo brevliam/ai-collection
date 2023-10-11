1302d193ca3b274385b70caa3ddd4fa17830eec6
PutraAlFarizi15 <farizip279@gmail.com>
2023-10-12 05:31:24 +0700

    change function in file Views.py and update urls

--- ai_collection/fitur_12/views.py
+++ ai_collection/fitur_12/views.py
@@ -4,25 +4,23 @@ from rest_framework import status
 from fitur_12.function.feature import predict_tenor, predict_loan
 
 # Create your views here.
-@api_view(["POST"])
-def recomendation_tenor(request):
-	try:
-		mydata = request.data
-		result = predict_tenor(mydata)
-		print(result)
-		return Response(build_result(result), status = status.HTTP_200_OK)
-	except ValueError as e:
-		return Response(e.args[0], status.HTTP_400_BAD_REQUEST)
-	
-@api_view(["POST"])
-def request_loan(request):
-	try:
-		mydata = request.data
-		result = predict_loan(mydata)
-		print(result)
-		return Response(build_result(result), status = status.HTTP_200_OK)
-	except ValueError as e:
-		return Response(e.args[0], status.HTTP_400_BAD_REQUEST)
+class RecommendationTenor(APIView):
+     def post(self, request, format=None):
+        try:
+            result = predict_tenor(request.data)
+            return Response(build_result(result), status = status.HTTP_200_OK)
+        except Exception as e:
+            error_message = str(e)
+            return Response({'error': error_message}, status=status.HTTP_400_BAD_REQUEST)
+        
+class RequestLoan(APIView):
+     def post(self, request, format=None):
+        try:
+            result = predict_loan(request.data)
+            return Response(build_result(result), status = status.HTTP_200_OK)
+        except Exception as e:
+            error_message = str(e)
+            return Response({'error': error_message}, status=status.HTTP_400_BAD_REQUEST)
 
 def build_result(result):
     result = {
