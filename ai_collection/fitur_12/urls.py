ab78509b28f3a386af1a9b2e3cc1eccae03fd969
PutraAlFarizi15 <farizip279@gmail.com>
2023-10-12 05:13:35 +0700

    Change function in file Views.py and update url.py

--- ai_collection/fitur_12/urls.py
+++ ai_collection/fitur_12/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import recomendation_tenor, request_loan
+from .views import RecommendationTenor, RequestLoan
 
 urlpatterns = [
-    path('recommendation-tenor/', recomendation_tenor),
-    path('request-loan/', request_loan)
+    path('recommendation-tenor/', RecommendationTenor.as_view(), 
+         name='recommendation_tenor'),
+    path('request-loan/', RequestLoan.as_view(), 
+         name='request_loan'),
 ]
