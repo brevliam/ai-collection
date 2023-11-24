8d937a7657fe1a6863b724766da8461fb7a703c8
abroid <abdurroid246@gmail.com>
2023-11-24 16:27:56 +0700

    refactor views & utils with pylint

--- ai_collection/fitur_19/libraries/utils.py
+++ ai_collection/fitur_19/libraries/utils.py
@@ -1,26 +1,70 @@
-# This module contains every method for appending new data to the dataset.
+"""
+Module: data_appender
+Description: Contains methods for appending new data to the dataset.
+"""
 
-import pandas as pd
 import os
+import pandas as pd
+
 
 def append_dataset_with_new_data(dataset_file_name, input_df, result):
-   dataset_path = load_dataset_path(dataset_file_name)
-   new_row = build_new_row(input_df, result)
-   append_new_row(dataset_path, new_row)
+    """
+    Append new data to the dataset.
+
+    Args:
+        dataset_file_name (str): The filename of the dataset.
+        input_df (pd.DataFrame): The input DataFrame.
+        result (dict): The result data to be appended.
+
+    Returns:
+        None
+    """
+    dataset_path = load_dataset_path(dataset_file_name)
+    new_row = build_new_row(input_df, result)
+    append_new_row(dataset_path, new_row)
 
 def load_dataset_path(filename):
-   feature_dir = os.path.dirname(os.path.abspath(__file__))
-   dataset_dir = os.path.join(feature_dir, "..", "dataset")
-   file_path = os.path.join(dataset_dir, filename)
-   
-   return file_path
+    """
+    Load the full path of the dataset file.
+
+    Args:
+        filename (str): The filename of the dataset.
+
+    Returns:
+        str: The full path of the dataset file.
+    """
+    feature_dir = os.path.dirname(os.path.abspath(__file__))
+    dataset_dir = os.path.join(feature_dir, "..", "dataset")
+    file_path = os.path.join(dataset_dir, filename)
+
+    return file_path
 
 def append_new_row(dataset_path, new_row):
-   new_row.to_csv(dataset_path, mode='a', header=False, index=False)
+    """
+    Append a new row to the dataset file.
+
+    Args:
+        dataset_path (str): The full path of the dataset file.
+        new_row (pd.DataFrame): The new row to be appended.
+
+    Returns:
+        None
+    """
+    new_row.to_csv(dataset_path, mode='a', header=False, index=False)
 
 def build_new_row(input_df, result):
-   result_mod_data = {key: [value] for key, value in result.items()}
-   result_df = pd.DataFrame(result_mod_data)
-   new_row = pd.concat([input_df, result_df], axis=1)
+    """
+    Build a new row by concatenating input DataFrame and result data.
 
-   return new_row
+    Args:
+        input_df (pd.DataFrame): The input DataFrame.
+        result (dict): The result data to be appended.
+
+    Returns:
+        pd.DataFrame: The new row.
+    """
+    result_mod_data = {key: [value] for key, value in result.items()}
+    result_df = pd.DataFrame(result_mod_data)
+    new_row = pd.concat([input_df, result_df], axis=1)
+
+    return new_row
--- ai_collection/fitur_19/views.py
+++ ai_collection/fitur_19/views.py
@@ -1,29 +1,26 @@
-from django.shortcuts import render
+"""
+Module untuk melakukan prediksi terkait fitur-fitur tertentu.
+"""
+
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 
-from fitur_19.function.feature import predict_default_payment, predict_recommended_collectors_supervision
+from fitur_19.function.feature import (
+    predict_default_payment,
+    predict_recommended_collectors_supervision
+)
 
-class DefaultPaymentPrediction(APIView):
-    def post(self, request, format=None):
-        try:
-            result = predict_default_payment(request.data)
-            return Response(build_result(result), status = status.HTTP_200_OK)
-        except Exception as e:
-            error_message = str(e)
-            return Response({'error': error_message}, status=status.HTTP_400_BAD_REQUEST)
-        
-class RecomendSupervision(APIView):
-    def post(self, request, format=None):
-        try:
-            result = predict_recommended_collectors_supervision(request.data)
-            return Response(build_result(result), status = status.HTTP_200_OK)
-        except Exception as e:
-            error_message = str(e)
-            return Response({'error': error_message}, status=status.HTTP_400_BAD_REQUEST)
-        
 def build_result(result):
+    """
+    Build a standardized result structure.
+
+    Args:
+        result (any): The result data to be included in the response.
+
+    Returns:
+        dict: A dictionary containing status, message, and result.
+    """
     result = {
         "status": 200,
         "message": "success",
@@ -31,3 +28,51 @@ def build_result(result):
     }
 
     return result
+
+class DefaultPaymentPrediction(APIView):
+    """
+    API view for predicting default payment.
+
+    Methods:
+        - post: Handles POST requests for predicting default payment.
+    """
+    def post(self, request):
+        """
+        Handle POST requests for predicting default payment.
+
+        Args:
+            request (rest_framework.request.Request): The incoming request.
+
+        Returns:
+            rest_framework.response.Response: The response containing the prediction result.
+        """
+        try:
+            result = predict_default_payment(request.data)
+            return Response(build_result(result), status=status.HTTP_200_OK)
+        except Exception as e:
+            error_message = str(e)
+            return Response({'error': error_message}, status=status.HTTP_400_BAD_REQUEST)
+
+class RecomendSupervision(APIView):
+    """
+    API view for predicting recommended collectors supervision.
+
+    Methods:
+        - post: Handles POST requests for predicting recommended collectors supervision.
+    """
+    def post(self, request):
+        """
+        Handle POST requests for predicting recommended collectors supervision.
+
+        Args:
+            request (rest_framework.request.Request): The incoming request.
+
+        Returns:
+            rest_framework.response.Response: The response containing the prediction result.
+        """
+        try:
+            result = predict_recommended_collectors_supervision(request.data)
+            return Response(build_result(result), status=status.HTTP_200_OK)
+        except Exception as e:
+            error_message = str(e)
+            return Response({'error': error_message}, status=status.HTTP_400_BAD_REQUEST)
