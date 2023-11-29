8098784e965bdeea7d2af1578befeaa6a0dc65b1
brevliam <97146830+brevliam@users.noreply.github.com>
2023-11-29 08:53:14 +0700

    bug fix: import error fitur_4 and fitur_18

--- ai_collection/fitur_18/urls.py
+++ ai_collection/fitur_18/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from .views import loss_reverse, time_to_collect, total_cost
+from .views import LossReverse, TimeToCollect, TotalCost
 
 urlpatterns = [
-    path('predict-loss-reverse/', loss_reverse.as_view(), name='Loss_Reverse_Forecast'),
-    path('predict-time-to-collect/', time_to_collect.as_view(), name='Loss_Reverse_Forecast'),
-    path('predict-total-cost/', total_cost.as_view(), name='Loss_Reverse_Forecast'),
+    path('predict-loss-reverse/', LossReverse.as_view(), name='Loss_Reverse_Forecast'),
+    path('predict-time-to-collect/', TimeToCollect.as_view(), name='Loss_Reverse_Forecast'),
+    path('predict-total-cost/', TotalCost.as_view(), name='Loss_Reverse_Forecast'),
 
 ]
 
--- ai_collection/fitur_4/urls.py
+++ ai_collection/fitur_4/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 from .views import AssignmentPrediction
-from .views import CampaignPrediciton
+from .views import CampaignPrediction
 
 
 urlpatterns = [
     path('predict-assignment/', AssignmentPrediction.as_view(), name='assignment_prediction'),
-    path('predict-campaign/', CampaignPrediciton.as_view(), name='campaign_prediction'),
+    path('predict-campaign/', CampaignPrediction.as_view(), name='campaign_prediction'),
 ]
