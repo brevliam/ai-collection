e44b50ecf5f9e94fa36e1a47fd5239c33674c4b9
abroid <abdurroid246@gmail.com>
2023-12-07 14:11:34 +0700

    refactor feature.py fitur_19

--- ai_collection/fitur_19/function/feature.py
+++ ai_collection/fitur_19/function/feature.py
@@ -1,46 +1,108 @@
-from ..apps import Fitur19Config
-from ..libraries import utils
+"""
+Module: AI Collection Rehabilitation
+
+This module provides functions for making predictions related to recommended collectors 
+and default payment.
+
+Classes:
+    - ObjectToCategoriesDebtor: Custom transformer for converting object columns to categorical 
+    columns for debtor data.
+    - ObjectToCategoriesCollector: Custom transformer for converting object columns to categorical 
+    columns for collector data.
+    - PreprocessDebtorForRecSys: Custom transformer for preprocessing debtor data for recommendation 
+    system.
+    - PreprocessCollectorForRecSys: Custom transformer for preprocessing collector data 
+    for recommendation system.
+
+Functions:
+    - predict_recommended_collectors_supervision: Predicts recommended collectors for 
+    supervision based on input data.
+    - predict_default_payment: Predicts default payment based on input data.
+
+Usage:
+    Call the provided functions to make predictions related to recommended collectors 
+    and default payment.
+
+"""
 from sklearn.base import BaseEstimator, TransformerMixin
 from sklearn.pipeline import Pipeline
 import numpy as np
-
 import pandas as pd
 
+from ..apps import Fitur19Config
+from ..libraries import utils
+
 def predict_recommended_collectors_supervision(data):
+    """
+    Predicts recommended collectors for supervision based on input data.
+
+    Parameters:
+    - data (dict): Input data dictionary.
+
+    Returns:
+    dict: Result of the prediction.
+    """
     model = Fitur19Config.recomended_model
-    DATASET_FILE_NAME_COLL = 'collectorr_df.csv'
+    dataset_file_name_coll = 'collectorr_df.csv'
 
     input_debtor_df = transform_input(data)
-    dataset_coll_path = utils.load_dataset_path(DATASET_FILE_NAME_COLL)
+    dataset_coll_path = utils.load_dataset_path(dataset_file_name_coll)
     dt_collector = pd.read_csv(dataset_coll_path)
 
     preprocessed_debtor = data_preprocessing_debtor.fit_transform(input_debtor_df)
     preprocessed_collector = data_preprocessing_collector.fit_transform(dt_collector)
 
     output = model.predict([preprocessed_debtor, preprocessed_collector])
-    result = transform_recommended_collectors_output(output, input_debtor_df, dt_collector)
-    
+    result = transform_recommended_collectors_output(output, dt_collector)
+
     return result
 
 def predict_default_payment(data):
+    """
+    Predicts default payment based on input data.
+
+    Parameters:
+    - data (dict): Input data dictionary.
+
+    Returns:
+    dict: Result of the prediction.
+    """
     model = Fitur19Config.default_payment_pred_model
-    DATASET_FILE_NAME = 'debtor_dum.csv'
+    dataset_file_name = 'debtor_dum.csv'
 
     input_df = transform_input(data)
     output = model.predict(input_df)
     result = transform_default_payment_pred_output(output)
     # result = output
-    utils.append_dataset_with_new_data(DATASET_FILE_NAME, input_df, result)
+    utils.append_dataset_with_new_data(dataset_file_name, input_df, result)
 
     return result
 
 def transform_input(data):
+    """
+    Transforms input data into a DataFrame.
+
+    Parameters:
+    - data (dict): Input data dictionary.
+
+    Returns:
+    pd.DataFrame: Transformed DataFrame.
+    """
     data = {key: [value] for key, value in data.items()}
     df = pd.DataFrame(data)
 
     return df
 
 def transform_default_payment_pred_output(pred):
+    """
+    Transforms default payment prediction output.
+
+    Parameters:
+    - pred (np.ndarray): Model prediction output.
+
+    Returns:
+    dict: Transformed result.
+    """
     default_payment_next_month = pred[0]
 
     if default_payment_next_month == 0:
@@ -55,87 +117,194 @@ def transform_default_payment_pred_output(pred):
     return data
 
 class ObjectToCategoriesDebtor(BaseEstimator, TransformerMixin):
-    def fit(self, X, y=None):
+    """
+    Custom transformer for converting object columns to categorical columns for debtor data.
+
+    Methods:
+        - fit(x, y=None): Fit the transformer.
+        - transform(x): Transform the input data.
+
+    Attributes:
+        None
+    """
+    def fit(self, x, y=None):
+        """
+        Fit method conforming to the sklearn transformer interface.
+
+        Parameters:
+        - x (pd.DataFrame): Input DataFrame.
+        - y: Unused parameter.
+
+        Returns:
+        self
+        """
         return self
 
-    def transform(self, X):
+    def transform(self, x):
+        """
+        Transform method conforming to the sklearn transformer interface.
+
+        Parameters:
+        - x (pd.DataFrame): Input DataFrame.
+
+        Returns:
+        pd.DataFrame: Transformed DataFrame.
+        """
         categorical_columns = ['debtor_education_level', 'action_code']
 
-        X[categorical_columns] = X[categorical_columns].astype('category')
+        x[categorical_columns] = x[categorical_columns].astype('category')
 
-        return X
+        return x
 
 class ObjectToCategoriesCollector(BaseEstimator, TransformerMixin):
-    def fit(self, X, y=None):
+    """
+    Custom transformer for converting object columns to categorical columns for collector data.
+
+    Methods:
+        - fit(x, y=None): Fit the transformer.
+        - transform(x): Transform the input data.
+
+    Attributes:
+        None
+    """
+    def fit(self, x, y=None):
+        """
+        Fit method conforming to the sklearn transformer interface.
+
+        Parameters:
+        - x (pd.DataFrame): Input DataFrame.
+        - y: Unused parameter.
+
+        Returns:
+        self
+        """
         return self
 
-    def transform(self, X):
+    def transform(self, x):
+        """
+        Transform method conforming to the sklearn transformer interface.
+
+        Parameters:
+        - x (pd.DataFrame): Input DataFrame.
+
+        Returns:
+        pd.DataFrame: Transformed DataFrame.
+        """
         categorical_columns = ['collector_education_level', 'action_code_history']
 
-        X[categorical_columns] = X[categorical_columns].astype('category')
+        x[categorical_columns] = x[categorical_columns].astype('category')
 
-        return X
+        return x
 
 
 class PreprocessDebtorForRecSys(BaseEstimator, TransformerMixin):
-    def fit(self, X, y=None):
+    """
+    Custom transformer for preprocessing debtor data for recommendation system.
+
+    Methods:
+        - fit(x, y=None): Fit the transformer.
+        - transform(x): Transform the input data.
+
+    Attributes:
+        None
+    """
+    def fit(self, x, y=None):
+        """
+        Fit method conforming to the sklearn transformer interface.
+
+        Parameters:
+        - x (pd.DataFrame): Input DataFrame.
+        - y: Unused parameter.
+
+        Returns:
+        self
+        """
         return self
 
-    def transform(self, X):
-        debt_edulvl = self._transform_debt_edu_level(X['debtor_education_level'])
-        debt_personality = self._transform_debt_personality(X['debtor_personality'])
-        action_code = self._transform_debt_action_code(X['action_code'])
+    def transform(self, x):
+        """
+        Transform method conforming to the sklearn transformer interface.
+
+        Parameters:
+        - x (pd.DataFrame): Input DataFrame.
+
+        Returns:
+        pd.DataFrame: Transformed DataFrame.
+        """
+        debt_edulvl = self._transform_debt_edu_level(x['debtor_education_level'])
+        debt_personality = self._transform_debt_personality(x['debtor_personality'])
+        action_code = self._transform_debt_action_code(x['action_code'])
         dt_debtor = pd.concat([debt_edulvl, debt_personality, action_code], axis=1)
         dt_debtor = pd.concat([dt_debtor] * 1000)
 
         return dt_debtor.reset_index(drop=True)
 
     def _transform_debt_edu_level(self, debt_edu_level):
+        """
+        Transform debtor education level into one-hot encoded DataFrame.
+
+        Parameters:
+        - debt_edu_level (pd.Series): Categorical values of debtor education level.
+
+        Returns:
+        pd.DataFrame: Transformed DataFrame.
+        """
         debt_edu_level_dum = []
         if debt_edu_level.values[0] == 'D3':
-          debt_edu_level_dum.append(1)
+            debt_edu_level_dum.append(1)
         else:
-          debt_edu_level_dum.append(0)
+            debt_edu_level_dum.append(0)
         if debt_edu_level.values[0] == 'S1':
-          debt_edu_level_dum.append(1)
+            debt_edu_level_dum.append(1)
         else:
-          debt_edu_level_dum.append(0)
+            debt_edu_level_dum.append(0)
         if debt_edu_level.values[0] == 'S2':
-          debt_edu_level_dum.append(1)
+            debt_edu_level_dum.append(1)
         else:
-          debt_edu_level_dum.append(0)
+            debt_edu_level_dum.append(0)
         if debt_edu_level.values[0] == 'SD':
-          debt_edu_level_dum.append(1)
+            debt_edu_level_dum.append(1)
         else:
-          debt_edu_level_dum.append(0)
+            debt_edu_level_dum.append(0)
         if debt_edu_level.values[0] == 'SMA':
-          debt_edu_level_dum.append(1)
+            debt_edu_level_dum.append(1)
         else:
-          debt_edu_level_dum.append(0)
+            debt_edu_level_dum.append(0)
         if debt_edu_level.values[0] == 'SMP':
-          debt_edu_level_dum.append(1)
+            debt_edu_level_dum.append(1)
         else:
-          debt_edu_level_dum.append(0)
+            debt_edu_level_dum.append(0)
 
 
         debt_edu_dum = np.array(debt_edu_level_dum).reshape((1, 6))
 
-        debt_edu_df = pd.DataFrame(debt_edu_dum, columns=['debtor_education_level_D3','debtor_education_level_S1',
-                                                          'debtor_education_level_S2','debtor_education_level_SD',
-                                                          'debtor_education_level_SMA','debtor_education_level_SMP'])
+        debt_edu_df = pd.DataFrame(debt_edu_dum,
+                                   columns=['debtor_education_level_D3','debtor_education_level_S1',
+                                            'debtor_education_level_S2','debtor_education_level_SD',
+                                            'debtor_education_level_SMA',
+                                            'debtor_education_level_SMP'])
 
         return debt_edu_df
 
     def _transform_debt_personality(self, debt_personality):
+        """
+        Transform debtor personality into one-hot encoded DataFrame.
+
+        Parameters:
+        - debt_personality (pd.Series): Categorical values of debtor personality.
+
+        Returns:
+        pd.DataFrame: Transformed DataFrame.
+        """
         debt_personality_dum = []
         if debt_personality.values[0] == 'kalem':
-          debt_personality_dum.append(1)
+            debt_personality_dum.append(1)
         else:
-          debt_personality_dum.append(0)
+            debt_personality_dum.append(0)
         if debt_personality.values[0] == 'sedang':
-          debt_personality_dum.append(1)
+            debt_personality_dum.append(1)
         else:
-          debt_personality_dum.append(0)
+            debt_personality_dum.append(0)
 
 
         debt_pers_dum = np.array(debt_personality_dum).reshape((1, 2))
@@ -146,75 +315,84 @@ class PreprocessDebtorForRecSys(BaseEstimator, TransformerMixin):
         return debt_pers_df
 
     def _transform_debt_action_code(self, debt_action_code):
+        """
+        Transform debtor action code into one-hot encoded DataFrame.
+
+        Parameters:
+        - debt_action_code (pd.Series): Categorical values of debtor action code.
+
+        Returns:
+        pd.DataFrame: Transformed DataFrame.
+        """
         debt_action_code_dum = []
         if debt_action_code.values[0] == 'ATPU':
-          debt_action_code_dum.append(1)
+            debt_action_code_dum.append(1)
         else:
-          debt_action_code_dum.append(0)
+            debt_action_code_dum.append(0)
         if debt_action_code.values[0] == 'BUSY':
-          debt_action_code_dum.append(1)
+            debt_action_code_dum.append(1)
         else:
-          debt_action_code_dum.append(0)
+            debt_action_code_dum.append(0)
         if debt_action_code.values[0] == 'CBLT':
-          debt_action_code_dum.append(1)
+            debt_action_code_dum.append(1)
         else:
-          debt_action_code_dum.append(0)
+            debt_action_code_dum.append(0)
         if debt_action_code.values[0] == 'DISC':
-          debt_action_code_dum.append(1)
+            debt_action_code_dum.append(1)
         else:
-          debt_action_code_dum.append(0)
+            debt_action_code_dum.append(0)
         if debt_action_code.values[0] == 'DISP':
-          debt_action_code_dum.append(1)
+            debt_action_code_dum.append(1)
         else:
-          debt_action_code_dum.append(0)
+            debt_action_code_dum.append(0)
         if debt_action_code.values[0] == 'FDBK':
-          debt_action_code_dum.append(1)
+            debt_action_code_dum.append(1)
         else:
-          debt_action_code_dum.append(0)
+            debt_action_code_dum.append(0)
         if debt_action_code.values[0] == 'HUPD':
-          debt_action_code_dum.append(1)
+            debt_action_code_dum.append(1)
         else:
-          debt_action_code_dum.append(0)
+            debt_action_code_dum.append(0)
         if debt_action_code.values[0] == 'INCO':
-          debt_action_code_dum.append(1)
+            debt_action_code_dum.append(1)
         else:
-          debt_action_code_dum.append(0)
+            debt_action_code_dum.append(0)
         if debt_action_code.values[0] == 'LMSG':
-          debt_action_code_dum.append(1)
+            debt_action_code_dum.append(1)
         else:
-          debt_action_code_dum.append(0)
+            debt_action_code_dum.append(0)
         if debt_action_code.values[0] == 'MUPD':
-          debt_action_code_dum.append(1)
+            debt_action_code_dum.append(1)
         else:
-          debt_action_code_dum.append(0)
+            debt_action_code_dum.append(0)
         if debt_action_code.values[0] == 'NOAN':
-          debt_action_code_dum.append(1)
+            debt_action_code_dum.append(1)
         else:
-          debt_action_code_dum.append(0)
+            debt_action_code_dum.append(0)
         if debt_action_code.values[0] == 'NOCO':
-          debt_action_code_dum.append(1)
+            debt_action_code_dum.append(1)
         else:
-          debt_action_code_dum.append(0)
+            debt_action_code_dum.append(0)
         if debt_action_code.values[0] == 'OTHR':
-          debt_action_code_dum.append(1)
+            debt_action_code_dum.append(1)
         else:
-          debt_action_code_dum.append(0)
+            debt_action_code_dum.append(0)
         if debt_action_code.values[0] == 'PTPR':
-          debt_action_code_dum.append(1)
+            debt_action_code_dum.append(1)
         else:
-          debt_action_code_dum.append(0)
+            debt_action_code_dum.append(0)
         if debt_action_code.values[0] == 'PTPY':
-          debt_action_code_dum.append(1)
+            debt_action_code_dum.append(1)
         else:
-          debt_action_code_dum.append(0)
+            debt_action_code_dum.append(0)
         if debt_action_code.values[0] == 'WRPH':
-          debt_action_code_dum.append(1)
+            debt_action_code_dum.append(1)
         else:
-          debt_action_code_dum.append(0)
+            debt_action_code_dum.append(0)
         if debt_action_code.values[0] == 'WUPD':
-          debt_action_code_dum.append(1)
+            debt_action_code_dum.append(1)
         else:
-          debt_action_code_dum.append(0)
+            debt_action_code_dum.append(0)
 
 
         debt_act_code_dum = np.array(debt_action_code_dum).reshape((1, 17))
@@ -242,18 +420,47 @@ class PreprocessDebtorForRecSys(BaseEstimator, TransformerMixin):
 
 
 class PreprocessCollectorForRecSys(BaseEstimator, TransformerMixin):
-    def fit(self, X, y=None):
+    """
+    Custom transformer for preprocessing collector data for the recommendation system.
+
+    Methods:
+        - fit(x, y=None): Fit the transformer.
+        - transform(x): Transform the input data.
+
+    Attributes:
+        None
+    """
+    def fit(self, x, y=None):
+        """
+        Fit method conforming to the sklearn transformer interface.
+
+        Parameters:
+        - x (pd.DataFrame): Input DataFrame.
+        - y: Unused parameter.
+
+        Returns:
+        self
+        """
         return self
 
-    def transform(self, X):
-        dt_collector = X[['collector_education_level','collector_personality','action_code_history']]
+    def transform(self, x):
+        """
+        Transform method conforming to the sklearn transformer interface.
+
+        Parameters:
+        - x (pd.DataFrame): Input DataFrame.
+
+        Returns:
+        pd.DataFrame: Transformed DataFrame.
+        """
+        dt_collector = x[['collector_education_level','collector_personality',
+                          'action_code_history']]
 
         dt_collector = pd.get_dummies(dt_collector, columns=['collector_education_level',
-                                                      'collector_personality','action_code_history'])
+                                                             'collector_personality',
+                                                             'action_code_history'])
 
         return dt_collector.reset_index(drop=True)
-
-   
 
 data_preprocessing_debtor = Pipeline([
     ('object_to_categories', ObjectToCategoriesDebtor()),
@@ -265,11 +472,16 @@ data_preprocessing_collector = Pipeline([
     ('preprocess_for_recsys', PreprocessCollectorForRecSys())
 ])
 
-def transform_recommended_collectors_output(output, input_debtor_df, dt_collector):
+def transform_recommended_collectors_output(output, dt_collector):
     output = pd.DataFrame(output)
     output.sort_values(by=[0], ascending=[False], inplace=True)
     recommended_collectors_index = output[:5].index
-    recommended_collectors = dt_collector.loc[recommended_collectors_index, 'collector_name'].tolist()
+    recommended_collectors = (
+        dt_collector
+        .loc[recommended_collectors_index, 'collector_name']
+        .tolist()
+    )
+
     result = {
         'recommended_collectors_to_monitor': recommended_collectors
     }
