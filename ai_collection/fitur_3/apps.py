fc972abe315f144cbc079c67e354b00c8dc2c561
Muhammad Reyhan Fitriyan <muhammadreyhanfitriyan@gmail.com>
2024-01-12 14:17:25 +0700

    Update apps.py to adapt to revised data and model changes

--- ai_collection/fitur_3/apps.py
+++ ai_collection/fitur_3/apps.py
@@ -10,22 +10,22 @@ class Fitur3Config(AppConfig):
     name = 'fitur_3'
 
     # Load model and pipeline for best collection time prediction
-    best_collection_time_model_path = os.path.join(os.path.dirname(__file__), 'model', 'best_collection_time_model.joblib')
+    best_collection_time_model_path = os.path.join(os.path.dirname(__file__), 'model', 'best_collection_time_model_v2.joblib')
     best_collection_time_model = joblib.load(best_collection_time_model_path)
 
-    best_collection_time_pipeline_path = os.path.join(os.path.dirname(__file__), 'model', 'best_collection_time_pipeline.joblib')
+    best_collection_time_pipeline_path = os.path.join(os.path.dirname(__file__), 'model', 'best_collection_time_pipeline_v2.joblib')
     best_collection_time_pipeline = joblib.load(best_collection_time_pipeline_path)
 
     # Load model and pipeline for best collection method prediction
-    best_collection_method_model_path = os.path.join(os.path.dirname(__file__), 'model', 'best_collection_method_model.joblib')
+    best_collection_method_model_path = os.path.join(os.path.dirname(__file__), 'model', 'best_collection_method_model_v2.joblib')
     best_collection_method_model = joblib.load(best_collection_method_model_path)
 
-    best_collection_method_pipeline_path = os.path.join(os.path.dirname(__file__), 'model', 'best_collection_method_pipeline.joblib')
+    best_collection_method_pipeline_path = os.path.join(os.path.dirname(__file__), 'model', 'best_collection_method_pipeline_v2.joblib')
     best_collection_method_pipeline = joblib.load(best_collection_method_pipeline_path)
 
     # Load model and pipeline for best collector prediction
-    best_collector_model_path = os.path.join(os.path.dirname(__file__), 'model', 'best_collector_model.h5')
+    best_collector_model_path = os.path.join(os.path.dirname(__file__), 'model', 'best_collector_model_v2.h5')
     best_collector_model = load_model(best_collector_model_path)
 
-    best_collector_pipeline_path = os.path.join(os.path.dirname(__file__), 'model', 'best_collector_pipeline.joblib')
+    best_collector_pipeline_path = os.path.join(os.path.dirname(__file__), 'model', 'best_collector_pipeline_v2.joblib')
     best_collector_pipeline = joblib.load(best_collector_pipeline_path)
